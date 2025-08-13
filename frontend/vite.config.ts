da5763c6c8427739fc61e03ae757ee4b799f6727
AndrePanin <ap@gear-tech.io>
2025-08-13 13:14:56 +0400

    Fixes after review

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,4 @@ import { viteConfigs } from '@gear-js/frontend-configs';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, mergeConfig } from 'vite';
 
-export default mergeConfig(
-  viteConfigs.app, 
-  defineConfig({ 
-    plugins: [tailwindcss()],
-    server: {
-      host: '0.0.0.0',
-      port: 3000,
-      strictPort: false,
-      open: false,
-    }
-  })
-);
+export default mergeConfig(viteConfigs.app, defineConfig({ plugins: [tailwindcss()] }));
