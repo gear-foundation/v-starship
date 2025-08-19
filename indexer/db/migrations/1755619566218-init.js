ba41aa9406bb2415b3dd60c14bb61ffd5018c551
Nikita Yutanov <nikitayutanov@gmail.com>
2025-08-19 20:06:29 +0400

    rename migration

--- indexer/db/migrations/1755619566218-init.js
+++ indexer/db/migrations/1755619566218-init.js
@@ -6,8 +6,8 @@
  * @class
  * @implements {MigrationInterface}
  */
-module.exports = class  $NAME1755554179789 {
-    name = ' $NAME1755554179789'
+module.exports = class Init1755619566218 {
+    name = 'Init1755619566218'
 
     async up(queryRunner) {
         await queryRunner.query(`CREATE TABLE "player" ("id" character varying NOT NULL, "score" integer NOT NULL, CONSTRAINT "PK_65edadc946a7faf4b638d5e8885" PRIMARY KEY ("id"))`);
