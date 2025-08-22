a2d39ce2ff8aa8fdc9e62a6f33cd1a890a037308
Nikita Yutanov <nikitayutanov@gmail.com>
2025-08-22 23:08:25 +0400

    add batching

--- indexer/src/handlers/player.ts
+++ indexer/src/handlers/player.ts
@@ -1,14 +1,16 @@
+import { In } from 'typeorm';
 import { isSailsEvent, isUserMessageSentEvent } from '../helpers';
 import { Player } from '../model';
 import { ProcessorContext } from '../processor';
 import { SailsDecoder } from '../sails-decoder';
-import { UserMessageSentEvent } from '../types';
 import { BaseHandler } from './base';
+
+type EventMessage = ReturnType<SailsDecoder['decodeEvent']>;
 
 const isObject = (value: unknown): value is Record<string, unknown> => typeof value === 'object' && value !== null;
 
-const getPlayerProps = (props: { address: string; score?: number; shipLevel?: number; name?: string }): Player => ({
-  id: props.address,
+const getPlayerProps = (props: { id: string; score?: number; shipLevel?: number; name?: string }): Player => ({
+  id: props.id,
   score: props.score ?? 0,
   shipLevel: props.shipLevel ?? 1,
   name: props.name ?? null,
@@ -33,104 +35,87 @@ export class PlayerHandler extends BaseHandler {
     await this._ctx.store.save(Array.from(values));
   }
 
-  private _updateScore(payload: unknown, ctx: ProcessorContext) {
-    if (!isObject(payload) || !('player' in payload) || !('points' in payload))
-      return ctx.log.error('Invalid payload structure for PointsAdded event');
+  private _isValidPayload(payload: unknown, key: string, method: string): payload is Record<string, unknown> {
+    if (!isObject(payload) || !(key in payload)) {
+      this._ctx.log.error(`Invalid payload structure for ${method} event. '${key}' key is missing`);
 
-    const playerAddress = String(payload.player);
-    const points = Number(payload.points);
-    const player = this._data.get(playerAddress);
-
-    if (player) {
-      const prevScore = player.score;
-
-      player.score += points;
-
-      ctx.log.info(`Player ${playerAddress} score updated: ${prevScore} -> ${player.score} (+${points})`);
-    } else {
-      const props = getPlayerProps({ address: playerAddress, score: points });
-
-      this._data.set(playerAddress, new Player(props));
-
-      ctx.log.info(`Player added as a result of PointsAdded event: ${JSON.stringify(props)}`);
+      return false;
     }
-  }
 
-  private _updateShipLevel(payload: unknown, ctx: ProcessorContext) {
-    if (!isObject(payload) || !('player' in payload) || !('level' in payload))
-      return ctx.log.error('Invalid payload structure for NewShipBought event');
-
-    const playerAddress = String(payload.player);
-    const level = Number(payload.level);
-    const player = this._data.get(playerAddress);
-
-    if (player) {
-      const prevShipLevel = player.shipLevel;
-
-      player.shipLevel = level;
-
-      ctx.log.info(`Player ${playerAddress} ship level updated: ${prevShipLevel} -> ${level}`);
-    } else {
-      const props = getPlayerProps({ address: playerAddress, shipLevel: level });
-
-      this._data.set(playerAddress, new Player(props));
-
-      ctx.log.warn(`Player added as a result of NewShipBought event: ${JSON.stringify(props)}`);
-    }
-  }
-
-  private _updateName(payload: unknown, ctx: ProcessorContext) {
-    if (!isObject(payload) || !('player' in payload) || !('name' in payload))
-      return ctx.log.error('Invalid payload structure for NameSet event');
-
-    const playerAddress = String(payload.player);
-    const name = String(payload.name);
-    const player = this._data.get(playerAddress);
-
-    if (player) {
-      const prevName = player.name;
-
-      player.name = name;
-
-      ctx.log.info(`Player ${playerAddress} name updated: ${prevName} -> ${name}`);
-    } else {
-      const props = getPlayerProps({ address: playerAddress, name });
-
-      this._data.set(playerAddress, new Player(props));
-
-      ctx.log.warn(`Player added as a result of NameSet event: ${JSON.stringify(props)}`);
-    }
-  }
-
-  private _updatePlayer(event: UserMessageSentEvent, ctx: ProcessorContext) {
-    if (!isSailsEvent(event)) return;
-
-    const { service, method, payload } = this._decoder.decodeEvent(event);
-
-    if (service !== 'Starship') return;
-
-    switch (method) {
-      case 'PointsAdded':
-        return this._updateScore(payload, ctx);
-      case 'NewShipBought':
-        return this._updateShipLevel(payload, ctx);
-      case 'NameSet':
-        return this._updateName(payload, ctx);
-    }
+    return true;
   }
 
   public async process(ctx: ProcessorContext) {
     await super.process(ctx);
 
-    const storedPlayers = await ctx.store.find(Player);
-    this._data = new Map(storedPlayers.map((player) => [player.id, player]));
+    const playerAddresses = new Set<string>();
+    const eventsToProcess = <{ event: EventMessage; playerAddress: string }[]>[];
 
     for (const block of ctx.blocks) {
       for (const event of block.events) {
-        if (!isUserMessageSentEvent(event)) continue;
+        if (!isUserMessageSentEvent(event) || !isSailsEvent(event)) continue;
 
-        this._updatePlayer(event, ctx);
+        const decodedEvent = this._decoder.decodeEvent(event);
+        const { service, method, payload } = decodedEvent;
+
+        if (
+          service !== 'Starship' ||
+          !['PointsAdded', 'NewShipBought', 'NameSet'].includes(method) ||
+          !this._isValidPayload(payload, 'player', method)
+        )
+          continue;
+
+        const playerAddress = String(payload.player);
+
+        playerAddresses.add(playerAddress);
+        eventsToProcess.push({ event: decodedEvent, playerAddress });
       }
+    }
+
+    if (playerAddresses.size !== 0) {
+      const ids = Array.from(playerAddresses);
+      const storedPlayers = await ctx.store.find(Player, { where: { id: In(ids) } });
+
+      this._data = new Map(storedPlayers.map((player) => [player.id, player]));
+    }
+
+    for (const { event, playerAddress } of eventsToProcess) {
+      const { method, payload } = event;
+
+      const existingPlayer = this._data.get(playerAddress);
+      const player = existingPlayer || new Player(getPlayerProps({ id: playerAddress }));
+      const prevPlayer = { ...player };
+
+      switch (method) {
+        case 'PointsAdded': {
+          if (!this._isValidPayload(payload, 'points', method)) break;
+
+          player.score += Number(payload.points);
+          break;
+        }
+
+        case 'NewShipBought': {
+          if (!this._isValidPayload(payload, 'level', method)) break;
+
+          player.shipLevel = Number(payload.level);
+          break;
+        }
+
+        case 'NameSet': {
+          if (!this._isValidPayload(payload, 'name', method)) break;
+
+          player.name = String(payload.name);
+          break;
+        }
+      }
+
+      if (existingPlayer) {
+        ctx.log.info(`Player ${player.id} updated: ${JSON.stringify(prevPlayer)} => ${JSON.stringify(player)}`);
+      } else {
+        ctx.log.info(`New player ${player.id} added: ${JSON.stringify(player)}`);
+      }
+
+      this._data.set(playerAddress, player);
     }
   }
 }
