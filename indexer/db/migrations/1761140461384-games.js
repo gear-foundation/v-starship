cce1068f86258407dae78d81b3201b78da6e75d0
Nikita Yutanov <nikitayutanov@gmail.com>
2025-10-22 17:41:36 +0400

    add migration

---
+++ indexer/db/migrations/1761140461384-games.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef {import('typeorm').MigrationInterface} MigrationInterface
+ */
+
+/**
+ * @class
+ * @implements {MigrationInterface}
+ */
+module.exports = class Games1761140461384 {
+    name = 'Games1761140461384'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE TABLE "game" ("id" character varying NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "player_address" character varying NOT NULL, "points" integer NOT NULL, CONSTRAINT "PK_352a30652cd352f552fef73dec5" PRIMARY KEY ("id"))`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`DROP TABLE "game"`);
+    }
+}
